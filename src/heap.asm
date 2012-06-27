21b6f273880310f01f097f9435e9cbf576b6a3a7
aghamir <moh.aghamir@gmail.com>
2012-06-27 10:38:26 +0430

    Update master

--- src/heap.asm
+++ src/heap.asm
@@ -1,30 +1,215 @@
-# Author: Aghamir
-.globl heap_init
-.globl heap_insert
-.globl heap_extract_min
-
 .text
-heap_init:
-	# initialize a min_heap of capacity n
-	# $a0: n
+.globl main
+main:
+	li $a0, 3
+	jal heap_init
+	li $a0, 4
+	li $a1, 123
+	jal heap_insert
+	li $a0, 2
+	li $a1, 1234
+	jal heap_insert
+	li $a0, 1
+	li $a1, 234
+	jal heap_insert
+	jal heap_extractmin
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	move $a0, $v1
+	li $v0, 1
+	syscall
 	
-	# impl:
-	# allocate two arrays of size n
-	# store their pointer in key_arr_ptr and val_arr_ptr
+	jal heap_extractmin
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	move $a0, $v1
+	li $v0, 1
+	syscall
+	li $ra, 0
 	jr $ra
+
+.globl heap_init
+heap_init:
+	li $v0, 9	
+	sll $a0, $a0, 2
+	syscall		
+	sw $v0, Heap_BaseKey	
+	li $v0, 9	
+	syscall
+	sw $v0, Heap_BaseValue		
+	jr $ra
+
+
+.globl heap_insert
 
 heap_insert:
-	# $a0: key
-	# $a1: value
+	addi $sp, $sp, -12
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	lw $s0, Heap_BaseKey
+	lw $s1, Heap_BaseValue
+	lw $t0, Heap_CurrentSize
+	sll $t1, $t0, 2
+	add $t1, $t1, $s0
+	sw $a0, 0($t1)
+	sll $t1, $t0, 2
+	add $t1, $t1, $s1
+	sw $a1, 0($t1)
+	addi $t0, $t0, 1
+	sw $t0, Heap_CurrentSize
+	sw $ra, 8($sp)
+	addi $a0, $t0, -1
+	jal Heap_InsertUpdate
+	lw $ra, 8($sp)
+	lw $s1, 4($sp)
+	lw $s0, 0($sp)
+	addi $sp, $sp, 12
+	jr $ra
+	
+	
+.globl heap_extractmin
+heap_extractmin:
+	addi $sp, $sp, -20
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	lw $s0, Heap_BaseKey
+	lw $s1, Heap_BaseValue
+	lw $t0, Heap_CurrentSize
+	addi $t0, $t0, -1
+	lw $v0, 4($s0)
+	lw $v1, 4($s1)
+	sll $t1, $t0, 2
+	add $t1, $t1, $s0
+	lw $t1, 0($t1)
+	sw $t1, 4($s0)
+	sll $t1, $t0, 2
+	add $t1, $t1, $s1
+	lw $t1, 0($t1)
+	sw $t1, 4($s1)
+	sw $t0, Heap_CurrentSize
+	sw $ra, 8($sp)
+	sw $v0, 12($sp)
+	sw $v1, 16($sp)
+	li $a0, 1
+	jal Heap_Update
+	lw $v1, 16($sp)
+	lw $v0, 12($sp)
+	lw $ra, 8($sp)
+	lw $s1, 4($sp)
+	lw $s0, 0($sp)
+	addi $sp, $sp, 20
 	jr $ra
 
-heap_extract_min:
-	# $v0: min_el.key
-	# $v1: min_el.value
+.globl Heap_Parrent
+Heap_Parrent:
+	srl $v0, $a0, 1
+	jr $ra
+	
+.globl Heap_LeftChild
+Heap_LeftChild:
+	sll $v0, $a0, 1
+	jr $ra
+	
+.globl Heap_RightChild
+Heap_RightChild:
+	sll $v0, $a0, 1
+	add $v0, $v0, 1
 	jr $ra
 
+.globl Heap_InsertUpdate
+Heap_InsertUpdate:
+	add $t0, $a0, $0
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	blt $t0, 2, Heap_IUD_End
+	Heap_IUD_Loop:
+		sll $t1, $t0, 2
+		add $t1, $s0, $t1
+		add $a0, $t0, $0		
+		jal Heap_Parrent
+		sll $v0, $v0, 2
+		add $t2, $s0, $v0
+		lw $t3, 0($t1)
+		lw $t4, 0($t2)
+		ble $t4, $t3, Heap_IUD_End
+		sw $t4, 0($t1)
+		sw $t3, 0($t2)
+		sll $t1, $t0, 2
+		add $t1, $s1, $t1
+		add $t2, $s1, $v0
+		lw $t3, 0($t1)
+		lw $t4, 0($t2)
+		sw $t4, 0($t1)
+		sw $t3, 0($t2)
+		srl $v0, $v0, 2
+		add $t0, $v0, $0
+		bgt $t0, 1, Heap_IUD_Loop
+	Heap_IUD_End:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+	
+	
+.globl Heap_Update
+Heap_Update:
+	add $v1, $a0, $0
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal Heap_LeftChild
+	add $t1, $v0, $0
+	jal Heap_RightChild
+	add $t2, $v0, $0
+	ble $t0, $t1, Heap_UD_secondIf
+	sll $t3, $a0, 2
+	sll $t4, $t1, 2
+	add $t3, $s0, $t3
+	add $t4, $s0, $t4
+	lw $t3, 0($t3)
+	lw $t4, 0($t4)
+	Heap_UD_firstIf:
+		ble $t3, $t4, Heap_UD_secondIf
+		add $v1, $t1, $0
+	Heap_UD_secondIf:
+		ble $t0, $t2, Heap_UD_thirdIf
+		sll $t3, $v1, 2
+		sll $t4, $t2, 2
+		add $t3, $s0, $t3
+		add $t4, $s0, $t4
+		lw $t3, 0($t3)
+		lw $t4, 0($t4)
+		ble $t3, $t4, Heap_UD_thirdIf
+		add $v1, $t1, $0
+	Heap_UD_thirdIf:
+		beq $v1, $a0, Heap_UD_End
+		sll $t1, $a0, 2
+		add $t1, $s0, $t1
+		sll $t2, $v1, 2
+		add $t2, $s0, $t2
+		lw $t3, 0($t1)
+		lw $t4, 0($t2)
+		sw $t4, 0($t2)
+		sw $t3, 0($t1)
+		sll $t1, $a0, 2
+		add $t1, $s1, $t1
+		sll $t2, $v1, 2
+		add $t2, $s1, $t2
+		lw $t3, 0($t1)
+		lw $t4, 0($t2)
+		sw $t4, 0($t2)
+		sw $t3, 0($t1)
+		add $a0, $v1, $0
+		jal Heap_Update
+	Heap_UD_End:
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		jr $ra
 .data
-key_arr_ptr:
-	.space 4
-val_arr_ptr:
-	.space 4
+Heap_BaseKey:	
+	.word 0
+Heap_BaseValue:		
+	.word 0
+Heap_CurrentSize:
+	.word 1	
+	
